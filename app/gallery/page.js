a4ef868b1f4ddd8de9760a5d17d38a63bd735f88
Zach Scott <codzapman@gmail.com>
2026-06-29 21:29:04 -0230

    Now retrieving data from MongoDB storage - (Error in the image formatting, will handle at a later date.)

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,26 +1,25 @@
-"use client";
+import { connectToDB } from '@/app/api/db';
+import { revalidatePath } from 'next/cache';
+import VehicleCard from '@/app/components/vehiclecard/vehiclecard';
 
-import { useEffect, useState } from "react";
-import VehicleCard from "@/app/components/vehiclecard/vehiclecard";
-import carInfo from "../../data/carinfo";
-
-export default function Gallery() {
-  const [submissions, setSubmissions] = useState([]);
-
-  useEffect(() => {
-    const info = localStorage.getItem("submittedCars");
-    setSubmissions(JSON.parse(info || "[]"));
-  }, []);
-
-  const both = [...carInfo, ...submissions];
+export default async function Gallery() {
+    const { db } = await connectToDB();
+    
+    const cars = await db.collection('cars').find({}).toArray();
+    
+    const carsWithStringId = cars.map(car => ({
+        ...car,
+        _id: car._id.toString(),
+        imageUrl: car.imageData
+    }));
 
   return (
-    <div className="space-y-8 py-10">
-      <ul className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
-        {both.map((vehicle) => (
-          <VehicleCard key={vehicle.id} vehicle={vehicle} />
-        ))}
-      </ul>
-    </div>
-  );
+        <div className="space-y-8 py-10">
+            <ul className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
+                {carsWithStringId.map((vehicle) => (
+                    <VehicleCard key={vehicle._id} vehicle={vehicle} />
+                ))}
+            </ul>
+        </div>
+    );
 }
